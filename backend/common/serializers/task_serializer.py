8c7dbebd3099dca73fe8c9882210eae8248d5268
Felipe Valente <gbdev02@gbringel.com>
2025-07-02 08:28:20 -0400

    fix: intergração do dashboard

--- backend/common/serializers/task_serializer.py
+++ backend/common/serializers/task_serializer.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 from common.models import (
-    Task
+    Task,
+    TaskUser
 )
 
 class TaskSerializer(serializers.ModelSerializer):
-    expiration_date = serializers.DateTimeField(required=True, allow_null=False)
+    expiration_date = serializers.DateTimeField(required=True, allow_null=False, format="%d/%m/%Y %H:%M")
+    
     class Meta:
         model = Task
         fields = ['id', 'title', 'description', 'expiration_date', 'status', 'priority']
+        
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        task_user = TaskUser.objects.filter(task=instance).first()
+        data['responsible'] = task_user.user.nome
+        return data
         
